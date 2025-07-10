cefcbc04715c9cb215484b9cb2072c0d2502a260
“Scott <scott@rhamy.com>
2025-07-10 17:31:35 -0400

    annotation issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { dateDifference, trim0decimals } from "$lib/utils";
-	import Tooltip from "$lib/Components/Tooltip.svelte";
+	import { Temporal } from "temporal-polyfill";
+	import SvelteTooltip from "$lib/Components/Tooltip.svelte";
 	import {
 		LineChart,
 		Rule,
@@ -13,15 +14,15 @@
 		Spline,
 		Text,
 		AnnotationLine,
+		Tooltip,
 		Points,
 		Tooltip as ChartTooltip,
+		Legend,
 	} from "layerchart";
-	import { SeriesState } from "/Users/sjr/Documents/Shared/Coding/Svelte/psa5/node_modules/layerchart/dist/components/charts/utils.svelte.js";
+
 	import { scaleTime, scaleSqrt, scaleLinear } from "d3-scale";
 	import { curveCatmullRom } from "d3-shape";
-	import { onMount } from "svelte";
-	import Dice_4 from "@lucide/svelte/icons/dice-4";
-	// import { date, format, PeriodType, sortFunc, styles } from "@layerstack/utils";
+	import { date, format, PeriodType, sortFunc, styles } from "@layerstack/utils";
 	// import { trim0decimals, unique } from "$lib/utils";
 	// import { Slider } from "@skeletonlabs/skeleton-svelte";
 	// import { X } from "@lucide/svelte";
@@ -91,14 +92,29 @@
 </label>
 <div class="border-primary-500/20 hover:border-primary-500/50 my-4 h-[500px] rounded-md border p-2">
 	<LineChart
+		grid={false}
 		padding={{ top: 20, bottom: 60, left: 40, right: 90 }}
+		xPadding={[16, 10]}
+		yPadding={[0, 5]}
 		data={psas}
 		x="date"
 		xScale={scaleTime()}
 		yScale={scaleSqrt()}
 		brush
 		legend
-		grid={false}
+		annotations={annots.map((a) => {
+			return {
+				type: "line",
+				label: a.test,
+				details: "aaaaa",
+				x: a.date,
+
+				props: {
+					line: { class: "stroke-error-500 [stroke-dasharray:2,2]" },
+					label: { class: "fill-error-500 text-[10px]" },
+				},
+			};
+		})}
 		{series}>
 		{#snippet marks({ context, visibleSeries, getSplineProps, getPointsProps })}
 			{#each visibleSeries as s, i (s.key)}
@@ -113,16 +129,16 @@
 					vertical>
 					{#snippet children({ gradient })}
 						<Spline stroke={gradient} curve={curveCatmullRom} {...getSplineProps(s, i)} />
-						<Points
+						<!-- <Points
 							r={5}
 							opacity={s.key === "psa" ? 100 : 0}
-							stroke={s.key === "psa" ? gradient : "transparent"} />
+							stroke={s.key === "psa" ? gradient : "transparent"} /> -->
 					{/snippet}
 				</LinearGradient>
 			{/each}
 		{/snippet}
 
-		{#snippet highlight({ context })}
+		<!-- {#snippet highlight({ context })}
 			{#if context.tooltip.data}
 				<Highlight
 					lines
@@ -133,7 +149,7 @@
 								: "var(--color-green-500)",
 					}} />
 			{/if}
-		{/snippet}
+		{/snippet} -->
 
 		{#snippet aboveMarks({
 			visibleSeries,
@@ -166,7 +182,7 @@
 						label: { class: `fill-${seriesref?.props.pcolor} opacity-40 text-[8px]` },
 					}} />
 			{/each}
-			{#if showannots}
+			<!-- {#if showannots}
 				{#each annots as anot}
 					<AnnotationLine
 						x={anot.date}
@@ -177,7 +193,7 @@
 							label: { class: "fill-success-500 opacity-40 text-[8px]" },
 						}} />
 				{/each}
-			{/if}
+			{/if} -->
 		{/snippet}
 
 		{#snippet axis({
@@ -185,51 +201,69 @@
 			visibleSeries,
 			series,
 		}: {
+			context: any;
 			visibleSeries: any;
 			series: any;
-			context: any;
 		})}
 			{#each visibleSeries as s (s.key)}
-				{@const sref = series.find((s: any) => s.key === s.key)}
+				{@const sref = series.find((ser: any) => s.key === ser.key)}
 				{@const ymax = Math.ceil(Math.max(...context.data.map((s: any) => s[s.key] ?? 0)))}
-
+				<!-- scale={scaleSqrt([0, ymax], [context.height, 0])} -->
 				<Axis
 					label={s.key.toUpperCase()}
 					placement={s.key === "psa" ? "left" : "right"}
 					format={(v) => v || ""}
+					ticks={(scale) => [...new Set([sref?.props.threshold, ...(scale.ticks?.() ?? [])])]}
 					rule={{ class: `stroke-${sref?.props?.pcolor}` }}
-					scale={scaleSqrt([0, ymax], [context.height, 0])}
 					labelProps={{
 						dx: s.key === "psa" ? 0 : -50,
 						class: `fill-${sref?.props.pcolor} stroke-transparent`,
 					}}
-					ticks={(scale) => [...new Set([sref?.props.threshold, ...(scale.ticks?.() ?? [])])]}
 					tickLabelProps={{
 						dx: s.key === "psa" ? -20 : 8,
 						textAnchor: "start",
 						class: `stroke-transparent fill-${sref?.props.pcolor}`,
 					}}
 					classes={{ tick: `stroke-${sref?.props.pcolor}` }}
-					class={`transition-all ${sref.key === "psadt" && visibleSeries.some((s) => s.key === "psavel") ? "translate-x-[50px]" : ""}`}
+					class={`transition-all ${sref.key === "psadt" && visibleSeries.some((s: any) => s.key === "psavel") ? "translate-x-[50px]" : ""}`}
 					tickLength={6} />
 			{/each}
 		{/snippet}
 
-		<!-- {#snippet tooltip({ context, series })}
-			{@const activeSeriesColor = series.find((s) => s.key === context.tooltip.data?.test)?.color}
-			<Tooltip.Root>
+		{#snippet tooltip({ context, visibleSeries, series })}
+			<Tooltip.Root class="rounded-xl p-4 shadow-xl" role="tooltip" aria-live="polite">
 				{#snippet children({ data })}
-					<Tooltip.Header>{format(context.x(data))}</Tooltip.Header>
-					<Tooltip.List>
-						<Tooltip.Item label={psas.test} value={data.value} color={activeSeriesColor} />
-					</Tooltip.List>
+					{#if data.annotation}
+						<div class="whitespace-nowrap">
+							{data.annotation.details}
+						</div>
+						<!-- <Tooltip.Header>
+							{context.data.annotation.test} on {format(context.data.annotation.date, "day")}
+						</Tooltip.Header>
+						<div class="whitespace-nowrap">
+							{@html context.data.annotation.value + " " + context.data.annotation.units}
+						</div> -->
+					{:else}
+						<Tooltip.Header class="justify-center; flex">{format(data.date, "day")}</Tooltip.Header>
+						<Tooltip.List>
+							{#each visibleSeries as s}
+								{@const sref = series.find((ser) => s.key === ser.key)}
+								{@const units =
+									sref?.key === "psa" ? " ng/ml" : sref?.key === "psadt" ? " years" : " ng/ml/year"}
+								<Tooltip.Item
+									label={s.key.toUpperCase()}
+									value={data[s.key] + units}
+									color={sref?.color} />
+							{/each}
+						</Tooltip.List>
+					{/if}
 				{/snippet}
 			</Tooltip.Root>
-		{/snippet} -->
+		{/snippet}
 	</LineChart>
 </div>
 
-<Tooltip>
+<SvelteTooltip>
 	{#snippet trigger()}Hover Me{/snippet}
 	{#snippet content()}This is a tooltip.{/snippet}
-</Tooltip>
+</SvelteTooltip>
